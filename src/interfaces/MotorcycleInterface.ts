8ce552f4f5be19b3cb0c1a212708c39f5cbe44d7
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-13 21:24:42 -0300

    Req 18

---
+++ src/interfaces/MotorcycleInterface.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+import { Vehicle } from './VehicleInterface';
+
+const MotorcycleSchema = z.object({
+  category: z.literal('Street')
+    .or(z.literal('Custom'))
+    .or(z.literal('Trail')),
+  engineCapacity: z.number({
+    required_error: 'engineCapacity is required',
+    invalid_type_error: 'engineCapacity must be a number',
+  }).min(0, { message: 'engineCapacity cannot be less than 0' })
+    .max(2500, { message: 'engineCapacity cannot be bigger than 7' }),
+});
+
+export type Motorcycle = Vehicle & z.infer<typeof MotorcycleSchema>;
+
+export { MotorcycleSchema };
